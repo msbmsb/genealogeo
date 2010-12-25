bec566a5ca5ecf07e750fe711edf2887a3ab7d89
Mitchell Bowden <mitchellbowden@gmail.com>
2010-12-25 01:35:24 -0600

    add kml build and marshalling to test

--- src/test/java/com/msbmsb/genealogeo/GenealoGeoTest.java
+++ src/test/java/com/msbmsb/genealogeo/GenealoGeoTest.java
@@ -9,8 +9,20 @@
 package com.msbmsb.genealogeo;
 
 import com.msbmsb.genealogeo.GenealoGeo;
+import com.msbmsb.genealogeo.kml.KmlBuilder;
+import com.msbmsb.genealoj.*;
+import com.msbmsb.gogogeocode.*;
+
+import de.micromata.opengis.kml.v_2_2_0.Document;
+import de.micromata.opengis.kml.v_2_2_0.Folder;
+import de.micromata.opengis.kml.v_2_2_0.Icon;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import de.micromata.opengis.kml.v_2_2_0.Style;
 
 import java.io.File;
+import java.util.List;
+import java.util.Scanner;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -52,7 +64,81 @@ public class GenealoGeoTest
         System.out.println("Output: ");
         genealoGeo.printFamilies();
         genealoGeo.printLocations();
+
+        // generate kml
+        KmlBuilder kmlBuilder = new KmlBuilder("ggExample1");
+        generateKml(kmlBuilder, genealoGeo);
         
         assert(true);
+    }
+
+    public void generateKml(KmlBuilder kmlBuilder, GenealoGeo genealoGeo)
+    {
+
+      List<GedcomNode> indivs = genealoGeo.gedcomParser.getNodes(Utils.INDIVIDUAL_TAG);
+      boolean validInput = false;
+      Scanner scanner = new Scanner(System.in);
+      while(!validInput) {
+        int j=1;
+        System.out.println("Please enter the number of the person (0 to quit)");
+        for(GedcomNode g : indivs) {
+          IndividualNode i = (IndividualNode) g;
+          String fullName = i.getFullName();
+          System.out.println(j + "\t" + fullName);
+          j++;
+        }
+  
+        System.out.println("> ");
+        int index = 3;
+        System.out.println("You entered: " + index);
+        if(index < 0 || index > j-1) {
+          System.out.println(index + " is an invalid entry.");
+          continue;
+        } else {
+          validInput = true;
+          kmlBuilder.addAndLink((IndividualNode) indivs.get(index-1));
+          kmlBuilder.marshal("test.kml");
+          break;
+        }
+      }
+
+    }
+
+    public void generateKmlForParents(Kml kml, Document doc, Folder folder, IndividualNode indiv) {
+      List<IndividualNode> parents = indiv.getParents();
+      Coordinates indiv_coords = GoGoGeocode.geocode(indiv.getChildrenWithTag("BIRT").get(0).getChildrenWithTag("PLAC").get(0).data());
+      for(IndividualNode i : parents) {
+        String name = i.getSurname();
+        String fullName = i.getFullName();
+        Style style = doc.createAndAddStyle();
+        style.withId("style_" + name)
+          .createAndSetIconStyle().withScale(10.0);
+        style.createAndSetLabelStyle().withColor("ff43b3ff").withScale(10.0);
+
+        List<GedcomNode> births = i.getChildrenWithTag("BIRT");
+        for(GedcomNode b : births) {
+          List<GedcomNode> locations = b.getChildrenWithTag("PLAC");
+    
+          if(locations == null) continue;
+    
+          for(GedcomNode l : locations) {
+            Coordinates coords = GoGoGeocode.geocode(l.data());
+            Placemark placemark = folder.createAndAddPlacemark();
+            placemark.withName(name)
+              .withStyleUrl("#style_" + name)
+              .withDescription(fullName + " - " + l.data())
+              .createAndSetLookAt().withLongitude(coords.longitude).withLatitude(coords.latitude).withAltitude(0).withRange(12000000);
+            placemark.createAndSetPoint().addToCoordinates(coords.longitude, coords.latitude);
+            placemark.createAndSetLineString().withExtrude(true).withTessellate(true)
+            .addToCoordinates(indiv_coords.toString())
+            .addToCoordinates(coords.toString());
+          }
+        }
+      }
+      
+      try {
+        kml.marshal(new File("test.kml"));
+      } catch(Exception e) {
+      }
     }
 }
